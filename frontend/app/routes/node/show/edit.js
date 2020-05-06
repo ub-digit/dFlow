3d82ec284be15f5bd806110057fb6b1741fe6f48
Per Larsson <per.larsson@ub.gu.se>
2020-05-06 20:05:13 +0200

    Fix indentation

--- frontend/app/routes/node/show/edit.js
+++ frontend/app/routes/node/show/edit.js
@@ -16,48 +16,46 @@ export default Ember.Route.extend({
           (errorObject) => {
             this.controller.set('error', errorObject.error);
           }
-          );
+        );
       }
     },
     updateNode: function(model) {
       var that = this;
-  	   // If we have a new_parent_id, ask user if it actually should be moved
-  	   if(model.new_parent_id && model.new_parent_id !== '') {
+       // If we have a new_parent_id, ask user if it actually should be moved
+       if(model.new_parent_id && model.new_parent_id !== '') {
         if(model.new_parent_id === 'root') {
           if(!this.get('session.data.authenticated.can_manage_tree_root')) {
            alert(this.get('i18n').t("nodes.move_root_denied"));
            return;
-         }
-         var should_save = confirm(this.get('i18n').t("nodes.move_confirm_root"));
-         if(should_save) {
-           model.parent_id = null;
-           delete model.new_parent_id;
-           this.send('saveNode', model);
-         }
-       } else {
-        this.store.find('treenode', model.new_parent_id, {
-         show_breadcrumb: true, show_breadcrumb_as_string: true
-       }).then(
-  			// Fetch parent we want to move object to
-  			function(new_model) {
-         var should_save = confirm(this.get('i18n').t("nodes.move_confirm") + "\n" + new_model.breadcrumb);
-         if(should_save) {
-          model.parent_id = model.new_parent_id;
-          delete model.new_parent_id;
-          that.send('saveNode', model);
+          }
+          var should_save = confirm(this.get('i18n').t("nodes.move_confirm_root"));
+          if(should_save) {
+            model.parent_id = null;
+            delete model.new_parent_id;
+            this.send('saveNode', model);
+          }
+        } else {
+          this.store.find('treenode', model.new_parent_id, {show_breadcrumb: true, show_breadcrumb_as_string: true}).then(
+            // Fetch parent we want to move object to
+            function(new_model) {
+              var should_save = confirm(this.get('i18n').t("nodes.move_confirm") + "\n" + new_model.breadcrumb);
+              if(should_save) {
+                model.parent_id = model.new_parent_id;
+                delete model.new_parent_id;
+                that.send('saveNode', model);
+              }
+            },
+            // Failed to fetch parent (no such node?)
+            function() {
+              alert(this.get('i18n').t("nodes.move_parent_not_found"));
+            }
+          );
         }
-      },
-  			// Failed to fetch parent (no such node?)
-  			function() {
-          alert(this.get('i18n').t("nodes.move_parent_not_found"));
-        }
-       );
-     }
-   } else {
-    this.send('saveNode', model);
-  }
-},
-saveNode: function(model) {
+      } else {
+        this.send('saveNode', model);
+      }
+    },
+    saveNode: function(model) {
       var that = this; // To be used in nested functions
       this.store.save('treenode', model).then(
         // Success function
@@ -65,11 +63,11 @@ saveNode: function(model) {
           that.send('refreshModel', model.parent_id); // Refresh children of current model
           that.transitionTo('node.show', model.parent_id);
         },
-		// Failed function
-    function(errorObject) {
-      that.controller.set('error', errorObject.error);
-    }
-    );
+        // Failed function
+        function(errorObject) {
+          that.controller.set('error', errorObject.error);
+        }
+      );
     }
   }
 });
