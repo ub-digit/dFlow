69a5c24a71faf821b2cd6af76cd27ac7eb12b196
Johan <johan.larsson@ub.gu.se>
2020-05-19 14:40:03 +0200

    added multiselect support for setting metadata

--- frontend/app/components/metadata-setter.js
+++ frontend/app/components/metadata-setter.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  page_type: undefined,
+  page_content: undefined,
+  select_all: true,
+  select_odd: true,
+  select_even: true,
+  latestSelected: null, 
+
+  hasSelected: Ember.computed('packageMetadata.images.@each.selected', function() {
+     return this.get("packageMetadata.images").filter(image => {
+        return image.selected;
+      }).length;
+  }),
 
   actions: {
     generatePageTypes() {
@@ -42,6 +54,61 @@ export default Ember.Component.extend({
           Ember.set(image, 'page_type', odd);
         }
       })
-    }
+    },
+
+    applyMetadataSequence() {
+      this.get('packageMetadata.images').filter(function(item) {return item.selected}).forEach((image, index) => {
+        var even = 'Undefined';
+        var odd = 'Undefined';
+        var currIndex = index;
+        switch (this.get('sequence')) {
+          case 'right-left':
+            even = 'RightPage';
+            odd = 'LeftPage';
+            break;
+          case 'left-right':
+            even = 'LeftPage';
+            odd = 'RightPage';
+            break;
+          default:
+            even = undefined;
+            odd = undefined;
+        }
+        if (currIndex % 2 === 0) {
+          Ember.set(image, 'page_type', even);
+        } else {
+          Ember.set(image, 'page_type', odd);
+        }
+      })
+    },
+    applyMetadataPhysical() {
+      this.get('packageMetadata.images').forEach((image, index) => {
+        if (image.selected) {
+          Ember.set(image, 'page_type', this.page_type);
+         // Ember.set(image, 'page_content', this.page_content);
+        }
+      })
+    },
+    applyMetadataLogical() {
+      this.get('packageMetadata.images').forEach((image, index) => {
+        if (image.selected) {
+          Ember.set(image, 'page_content', this.page_content);
+        }
+      })
+    },
+    selectAll() {
+      this.get('packageMetadata.images').forEach((image, index) => {
+          Ember.set(image, 'selected', true);        
+      })
+      this.set("latestSelected", null);
+    },
+
+    deselectAll() {
+      this.get('packageMetadata.images').forEach((image, index) => {
+        Ember.set(image, 'selected', false);        
+      })
+    },
+
+
   }
 });
